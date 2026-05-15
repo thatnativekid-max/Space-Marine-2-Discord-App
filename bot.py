735f93c420e274c897412ec9e253d7ab6f3cae5f
thatnativekid-max <thatnativekid@gmail.com>
2026-05-15 14:28:48 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.members = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
